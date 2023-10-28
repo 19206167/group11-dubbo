ff93e387c4d830df616154aec80539952361ced7
Liu Zijian <liuzijian_2021@163.com>
2023-10-28 19:54:49 +0800

    conflicts

---
+++ common-utils/src/main/java/com/example/group11/commons/utils/AliOssProperties.java
@@ -0,0 +1,10 @@
+package com.example.group11.commons.utils;/**
+* FileName: AliOssProperties.java
+* @Description ToDo
+*
+* @author 刘梓健
+* @Date    2023/10/28 15:36
+* @version 1.0
+*/
+public class AliOssProperties {
+}
---
+++ common-utils/src/main/java/com/example/group11/commons/utils/OssFileUpload.java
@@ -0,0 +1,10 @@
+package com.example.group11.commons.utils;/**
+* FileName: OssFileUpload.java
+* @Description ToDo
+*
+* @author 刘梓健
+* @Date    2023/10/28 15:23
+* @version 1.0
+*/
+public class OssFileUpload {
+}
