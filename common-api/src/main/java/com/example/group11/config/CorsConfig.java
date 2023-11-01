884d14277ddc25ce891570511c730ff7be6e422f
abc9090 <598985961@qq.com>
2023-11-02 00:36:11 +0800

    cross origin

--- common-api/src/main/java/com/example/group11/config/CorsConfig.java
+++ common-api/src/main/java/com/example/group11/config/CorsConfig.java
@@ -15,7 +15,13 @@ public class CorsConfig extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        response.addHeader("Access-Control-Allow-Credentials", "true");
+        response.addHeader("Access-Control-Expose-Headers", "Authorization");
+        response.addHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, X-Requested-By, If-Modified-Since, X-File-Name, X-File-Type, Cache-Control, Origin");
         response.addHeader("Access-Control-Allow-Origin", "*");
+        response.addHeader("Vary", "Origin");
+        response.addHeader("Vary", "Access-Control-Allow-Method");
+        response.addHeader("Vary", "Access-Control-Request-Headers");
         filterChain.doFilter(request, response);
     }
 }
