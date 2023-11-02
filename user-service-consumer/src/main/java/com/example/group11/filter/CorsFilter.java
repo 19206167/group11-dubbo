be9a39bbff0c3ad51a07d8155d92c5e6e4004979
abc9090 <598985961@qq.com>
2023-11-02 11:46:29 +0800

    前端跨域问题

--- user-service-consumer/src/main/java/com/example/group11/filter/CorsFilter.java
+++ user-service-consumer/src/main/java/com/example/group11/filter/CorsFilter.java
@@ -1,6 +1,8 @@
 package com.example.group11.filter;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
@@ -22,6 +24,10 @@ public class CorsFilter implements Filter {
         httpServletResponse.setHeader("Access-Control-Allow-Methods", "OPTIONS, POST, GET, PATCH, DELETE, PUT");
         httpServletResponse.setHeader("Access-Control-Max-Age", "3600");
         httpServletResponse.setHeader("Access-Control-Allow-Headers", allowHeaders);
+        if (servletRequest.getMethod().equals(RequestMethod.OPTIONS.name())) {
+            httpServletResponse.setStatus(HttpStatus.OK.value());
+            return;
+        }
         chain.doFilter(request, response);
     }
 }
