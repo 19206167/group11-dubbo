e0275204d3467a580972a92a8fcbeecdde1c7e27
abc9090 <598985961@qq.com>
2023-11-07 23:18:10 +0800

    充值修改url

--- payment-service-consumer/src/main/java/com/example/group11/controller/transaction/TransactionController.java
+++ payment-service-consumer/src/main/java/com/example/group11/controller/transaction/TransactionController.java
@@ -5,8 +5,8 @@ import com.example.group11.commons.utils.RestResult;
 import com.example.group11.service.transaction.TransactionService;
 import io.swagger.annotations.ApiOperation;
 import org.apache.dubbo.config.annotation.DubboReference;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
@@ -21,15 +21,16 @@ import java.math.BigDecimal;
  * @Date 2023/10/11 21:01
  */
 @RestController
+@RequestMapping("/transaction")
 public class TransactionController {
     @DubboReference(version = "1.0.0", interfaceClass = com.example.group11.service.transaction.TransactionService.class, timeout = 10000)
     TransactionService transactionService;
 
 
-    @GetMapping("/transaction/topUp")
-        @ApiOperation(value = "用户为账户充值", tags = "交易")
-        public RestResult topUpAccount(BigDecimal amount, HttpServletRequest httpServletRequest) {
-            // 获取当前用户id
+    @GetMapping("/topUp")
+    @ApiOperation(value = "用户为账户充值", tags = "交易")
+    public RestResult topUpAccount(BigDecimal amount, HttpServletRequest httpServletRequest) {
+        // 获取当前用户id
         String token = JWTUtil.getToken(httpServletRequest);
         Long userId = JWTUtil.getUserId(token);
 
